87b0a9e4f952f7f0604ec76ee701175a43ac51b0
SoniaJayat <SoniaJayat03@gmail.com>
2024-01-10 15:14:55 +0000

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -36,5 +36,22 @@ function updateTime() {
   sydneyTimeElement.innerHTML = `${sydneyTime.format("h:mm:ss A")}`;
 }
 
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = ` <div class="city" >
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("dddd Do MMMM YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss A")}</div>
+        </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
+
 updateTime();
 setInterval(updateTime, 1000);
